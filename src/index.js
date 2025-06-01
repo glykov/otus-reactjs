38a4fb1d25679d994eaab9b2349ad7aaa877974b
glykov <glebdom@gmail.com>
2025-06-01 12:34:11 +0300

    feat: Add momentjs to project. Show current date and time to user

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
+const moment = require("moment");
+
 console.log('first homework');
 console.log('Hello from Node console application!');
+const now = moment().format('MMMM Do YYYY, h:mm:ss a');
+console.log(`It is ${now} now`);
